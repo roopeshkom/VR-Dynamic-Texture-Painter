d478928314dbcc309b4a8f4d697c1032de590c68
Roopeswar Kommalapati <roopeshkom@gmail.com>
2016-06-29 12:10:42 -0400

    Got basic code working. The pixels that are changed are too small to notice normally. Now working on chnaging the pixels surrounding the texture coodinate hit by the raycast.

---
+++ _Scenes/test.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+	
+	void Update() {
+		if (Input.GetMouseButton (0)) {
+
+			RaycastHit hit;
+			if (Physics.Raycast (Camera.main.ScreenPointToRay (Input.mousePosition), out hit, 100f)) {
+
+				Renderer rend = hit.transform.GetComponent<Renderer> ();
+				MeshCollider meshCollider = hit.collider as MeshCollider;
+
+				if (rend == null) {
+					Debug.Log ("no rend");
+				}
+				else if(rend.sharedMaterial == null){
+					Debug.Log("no shared mat");
+				}
+				else if(rend.sharedMaterial.mainTexture == null){
+					Debug.Log("no main tex");
+				}
+				else if(meshCollider == null){
+					Debug.Log("no mesh collider");
+				}
+				else {
+					Debug.Log (hit.textureCoord);
+					Texture2D tex = rend.material.mainTexture as Texture2D;
+					Vector2 pixelUV = hit.textureCoord;
+					pixelUV.x *= tex.width;
+					pixelUV.y *= tex.height;
+
+
+					tex.SetPixel ((int)pixelUV.x, (int)pixelUV.y, Color.red);
+					tex.Apply ();
+				}
+			}
+		}
+	}
+}
