6a05b1f2946f74355d0ce53188219841c615a235
Roopeswar Kommalapati <roopeshkom@gmail.com>
2016-06-29 16:44:53 -0400

    Added sphere spinning,a dn public color selection, and widened color tooltip. Now working on integrating leap motion input into the program, with the detector scripts.

---
+++ _Scripts/ObjectPainter.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectPainter : MonoBehaviour {
+
+	public Color col;
+
+	void Update() {
+		if (Input.GetMouseButton (0)) {
+
+			RaycastHit hit;
+			if (Physics.Raycast (Camera.main.ScreenPointToRay (Input.mousePosition), out hit, 100f)) {
+
+				Renderer rend = hit.transform.GetComponent<Renderer> ();
+				MeshCollider meshCollider = hit.collider as MeshCollider;
+
+				if (rend == null) {
+					Debug.Log ("no rend");
+				}
+				else if(rend.sharedMaterial == null){
+					Debug.Log("no shared mat");
+				}
+				else if(rend.sharedMaterial.mainTexture == null){
+					Debug.Log("no main tex");
+				}
+				else if(meshCollider == null){
+					Debug.Log("no mesh collider");
+				}
+				else {
+					Debug.Log (hit.textureCoord);
+					Texture2D tex = rend.material.mainTexture as Texture2D;
+					Vector2 pixelUV = hit.textureCoord;
+					pixelUV.x *= tex.width;
+					pixelUV.y *= tex.height;
+
+					int xPix = (int)pixelUV.x;
+					int yPix = (int)pixelUV.y;
+
+					for (int i = 0; i < 29; i++) {
+						for (int j = 0; j < 29; j++) {
+							tex.SetPixel (xPix + i, yPix + j, col);
+						}
+					}
+
+					tex.Apply ();
+				}
+			}
+		}
+	}
+}
---
+++ _Scripts/SpinController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpinController : MonoBehaviour {
+
+	public bool spin = true;
+	
+	// Update is called once per frame
+	void Update () {
+		if (spin) {
+			transform.Rotate (0, 1f, 0);
+		}
+	}
+}
